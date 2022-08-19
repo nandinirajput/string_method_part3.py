63fcda800ee21fd8aaa334341dfd760150663221
RajputNandini <109655286+nandinirajput@users.noreply.github.com>
2022-08-19 14:16:27 +0530

    Add files via upload

---
+++ string_methods_part3.py
@@ -0,0 +1,24 @@
+# a="hello","python","programming"
+# print(type(a))  #tuple
+# b="  world  " join(a) #if we give space tp world
+# print(b) #hello world python world programming.this join method joins the two strings.
+# c="hello","python","programming"
+# d="world".join(c)
+# print(d)  #helloworldpythonworldprogramming.when we will not give any space so the output will be like this
+
+# e="hey"
+# f="*".join(e)
+# print(f) #h*e*y
+# g="bye"
+# h="6".join(g)
+# print(h) #b6y6e
+
+# i="hey",
+# j="$".join(i)
+# print(j) #hey.coz after comma(,)some thing string should be their orelse the output hits same back.
+# i="hey"," "
+# j="$".join(i)
+# print(j) #hey$. after comma their is an empty str so it give correct output.noy only empty str it can any stri
+
+
+# a="python programming"
